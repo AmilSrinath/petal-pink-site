6b98ebe8d7b1146541b97903b17516a47faefba0
AmilSrinath <amilsrinath5@gmail.com>
2025-03-03 13:14:26 +0530

    change ip address

--- src/containers/PageAbout/PageAbout.tsx
+++ src/containers/PageAbout/PageAbout.tsx
@@ -33,7 +33,7 @@ const PageAbout: FC<PageAboutProps> = ({ className = "" }) => {
           subHeading="We’re impartial and independent, and every day we create distinctive, world-class programmes and content which inform, educate and entertain millions of people in the around the world."
         />
 
-        {/*<SectionFounder />*/}
+        <SectionFounder />
         <div className="relative py-16">
           <BackgroundSection />
           <SectionClientSay />
