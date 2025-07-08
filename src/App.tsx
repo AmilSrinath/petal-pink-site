c7ce18ed340740bab271866493ad276239ae241b
AmilSrinath <amilsrinath5@gmail.com>
2025-07-08 09:58:23 +0530

    change ui

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,11 @@ function App() {
       <Helmet>
         <title>Petal Pink</title>
         <meta name="description" content="Petal Pink" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       </Helmet>
 
       {/* MAIN APP */}
-      <div className="bg-gradient-to-br from-white to-purple-50 dark:from-slate-900 dark:to-purple-900 text-base text-slate-900 dark:text-slate-200 min-h-screen">
+      <div className="bg-white text-base dark:bg-slate-900 text-slate-900 dark:text-slate-200 min-h-screen w-full overflow-x-hidden">
         <MyRouter />
       </div>
     </HelmetProvider>
